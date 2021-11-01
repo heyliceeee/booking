b73e6f83da07bf7f1d4bc01a849c0e6ae81b7747
heyliceeee <alicedias2002@hotmail.com>
2021-11-01 12:31:42 +0000

    01.11.2021

---
+++ server/rooms.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+function RoomRouter() {
+    let router = express();
+
+    router.use(express.json({ limit: '100mb' }));
+    router.use(express.urlencoded({ limit: '100mb', extended: true }));
+}
