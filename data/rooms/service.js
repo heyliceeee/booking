883c124fbf7e23d1058df2a941f49e21a15e39be
heyliceeee <alicedias2002@hotmail.com>
2021-11-06 12:52:17 +0000

    06.11.2021

--- data/rooms/service.js
+++ data/rooms/service.js
@@ -3,11 +3,16 @@
 function RoomService(RoomModel){
     let service = {
         create,
-        findAll
+        findAll,
+        findById,
+        update,
+        removeById
     };
+
 
     //criar room
     function create(values){
+
         let newRoom = RoomModel(values);
         return save(newRoom); //guarda novo room
     }
@@ -25,14 +30,56 @@ function RoomService(RoomModel){
         });
     }
 
-    //procurar
+    //procurar room
     function findAll(){
         return new Promise(function (resolve, reject){
+
             RoomModel.find({}, function (err, users) {
                 if (err) reject(err);
 
                 //objecto de todos os users
                 resolve(users);
+            });
+        });
+    }
+
+    //procurar room por id
+    function findById(id){
+        return new Promise(function (resolve, reject) {
+            RoomModel.findById(id, function (err, user) {
+                if(err) reject(err);
+
+                //objecto de todos os users
+                resolve(user);
+            });
+        });
+    }
+
+    //atualizar room
+    function update(id, values){
+        return new Promise(function (resolve, reject) {
+
+            //values - {description: quarto com vista mar} || {nAdult: 2} ... || {description: j, nAdult: 0} ...
+            RoomModel.findByIdAndUpdate(id, values, function (err, user) {
+                if(err) reject(err);
+
+                resolve(user);
+            });
+        });
+    }
+
+    //remover room pelo id
+    function removeById(id) {
+        return new Promise(function (resolve, reject) {
+
+            console.log(id);
+
+            RoomModel.findByIdAndRemove(id, function(err) {
+
+                
+                if(err) reject(err);
+                console.log(err);
+                resolve();
             });
         });
     }
--- server/rooms.js
+++ server/rooms.js
@@ -64,6 +64,85 @@ function RoomRouter() {
         });
 
 
+        router.route('/rooms/:roomId')
+            //GET - findById room
+            .get(function (req, res, next) {
+                let roomId = req.params['roomId'];
+
+                Rooms.findById(roomId)
+                    .then((room) => {
+                        console.log('---|find one room by ID|---'); //retorna o room pelo Id
+                        res.status(200);
+                        res.send(room);
+                        next();
+                    })
+
+                    .catch((err) => {
+                        console.log('"---|error|---"');
+                        res.status(404);
+                        next();
+                    });
+            })
+
+            //PUT - update room by ID
+            .put(function (req, res, next) {
+                let roomId = req.params['roomId'];
+                let body = req.body;
+
+                Rooms.update(roomId, body)
+                    .then((room) => {
+                        console.log('---|update one room by ID|---'); //altera dados do room
+                        res.status(200);
+                        res.send(room);
+                        next();
+                    })
+
+                    .catch((err) => {
+                        console.log('"---|error|---"');
+                        res.status(404);
+                        next();
+                    });
+            })
+
+            //DELETE - delete room by ID
+            .delete(function (req, res, next) {
+                let roomId = req.params['roomId'];
+
+                Rooms.removeById(roomId)
+                    .then(() => {
+                        console.log("---|delete one room by ID|---")
+                        res.status(200);
+                        next();
+                    })
+
+                    .catch((err) => {
+                        console.log('"---|error|---"');
+                        res.status(404);
+                        next();
+                    });
+            });
+
+
+            router.route('/rooms/:roomId/tags')
+                 //GET - findById room return tags
+                 .get(function (req, res, next) {
+                    let roomId = req.params['roomId'];
+                    let tags = req.body.tags;
+    
+                    Rooms.findById(roomId)
+                        .then((room) => {
+                            console.log('---|find one room by ID return tags|---'); //retorna as tags do room pelo Id
+                            res.status(200);
+                            res.send(tags);
+                            next();
+                        })
+    
+                        .catch((err) => {
+                            console.log('"---|error|---"');
+                            res.status(404);
+                            next();
+                        });
+                })
 
 
     router.route('/hotel')
