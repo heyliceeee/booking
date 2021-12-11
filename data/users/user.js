b1990ed7465f000f4a8294efe9e7147b3756efa0
heyliceeee <alicedias2002@hotmail.com>
2021-12-11 11:46:57 +0000

    update users schema

--- data/users/user.js
+++ data/users/user.js
@@ -1,12 +1,25 @@
 let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
+const scopes = require('./scopes');
+
+let RoleSchema = new Schema({
+    name: { type: String, required: true, unique: true },
+    scopes: [{ type: String, enum: 
+        [
+            scopes["read-own-reserves'"], scopes["update-own-reserve"], 
+            scopes["read-users"],
+            scopes["update-reserve"], scopes["read-reserves"], scopes["delete-reserve"], scopes["create-room"], scopes["update-room"], scopes["read-reserve-client"],
+            scopes["create-reserve"], scopes["detail-reserve"], scopes["verify-logged-in"]
+        ] 
+    }]
+});
 
 //create a schema
 let UserSchema = new Schema({
-    name: {type: String, required: true, unique: true},
-    email: { type: String, required: true},
-    password: { type: String, required: true},
-    role : { type: String, default: "user", enum: ["user", "editor",  "admin"]}
+    name: {type: String, required: true, unique: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    role : { type: RoleSchema }
 });
 
 //the schema is useless so far
